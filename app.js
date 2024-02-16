fb7a1d02b06eb7172c26e0ba4d5ed7f85a0c4b06
dst327 <135099397+dst327@users.noreply.github.com>
2024-02-16 16:18:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const body_parser = require("body-parser");
 const commonRouter = require("./server/router/common");
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||3000;;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
